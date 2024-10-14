7640e80b21bc1f0b0a3f8b5ade649ce10cf379c6
Dilan Fernando <dilanmadushanka6013@gmail.com>
2024-10-14 20:15:56 +0530

    init commit

---
+++ assets/Customer.js
@@ -0,0 +1,26 @@
+function setCustomerIds(){
+    $.ajax({
+        method:"GET",
+        url:"http://localhost:8080/Spring_Pos/api/v1/customer",
+        success: function(customers){
+
+            customers.forEach(customer => {
+                
+                $('#cusId').append(`<option value="${customer.cusId}">${customer.cusId}</option>`)
+                
+            });
+
+        },
+        error: function(customers){
+            console.log(customers);
+            
+        }
+    })
+}
+
+function genarateNextCustomerId(){
+    $.ajax({
+        method:"GET",
+        url:""
+    })
+}
---
+++ assets/navigation/spa.js
@@ -0,0 +1,40 @@
+$('#customerButton').on('click', function(){
+
+    $('#customerSection').css({display:'block'});
+    $('#itemSection').css({display:'none'});
+    $('#orderSection').css({display:'none'});
+    $('#cardSection').css({display:'none'});
+    setCustomerIds();
+
+});
+
+$('#itemButton').on('click', function(){
+
+    $('#itemSection').css({display:'block'});
+    $('#customerSection').css({display:'none'});
+    $('#orderSection').css({display:'none'});
+    $('#cardSection').css({display:'none'});
+
+});
+
+$('#orderButton').on('click', function(){
+
+    $('#orderSection').css({display:'block'});
+    $('#customerSection').css({display:'none'});
+    $('#itemSection').css({display:'none'});
+    $('#cardSection').css({display:'none'});
+
+});
+
+$(document).ready(function(){
+    $('#orderSection').css({display:'none'});
+    $('#customerSection').css({display:'none'});
+    $('#itemSection').css({display:'none'});
+    $('#cardSection').css({display:'block'});
+})
+
+
+
+
+
+
