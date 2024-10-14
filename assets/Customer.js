eef6d7f003cb0ef7e050de6c96034b203a0db4f5
Dilan Fernando <dilanmadushanka6013@gmail.com>
2024-10-14 22:40:57 +0530

    table clisk event is done

--- assets/Customer.js
+++ assets/Customer.js
@@ -63,9 +63,9 @@ function setCustomerTableValues(){
             customers.forEach(customer => {
                 
                 $('#customerTable').append(`<tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                     ${customer.cusId}
-                </th>
+                </td>
                 <td class="px-6 py-4">${customer.name}</td>
                 <td class="px-6 py-4">${customer.address}</td>
                 <td class="px-6 py-4">${customer.tel}</td>
@@ -175,3 +175,22 @@ $("#updateCustomer").on('click' ,function(){
     })
 
 })
+
+$('#customerTable').on('click', 'tr', function() {
+    const cusId = $(this).find('td').eq(0).text().trim();  
+    
+    $.ajax({
+        method:"GET",
+        url:`http://localhost:8080/Spring_Pos/api/v1/customer/${cusId}`,
+        success: function(customer){        
+            $('#cusId').val(customer.cusId);
+            $('#cusName').val(customer.name);
+            $('#cusAddress').val(customer.address);
+            $('#cusTel').val(customer.tel);
+
+        },
+        error: function(customer){
+            console.log(customer);
+        }
+    })
+});
